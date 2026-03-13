d0442230310bd5205e32ddc24cd09c178d6336c7
Anton Korobeynikov <anton@korobeynikov.info>
2026-03-13 15:39:35 -0700

    Enable Array.subscript._modify differentiation

--- lib/Serialization/DeserializeSIL.cpp
+++ lib/Serialization/DeserializeSIL.cpp
@@ -5348,7 +5348,8 @@ SILDeserializer::readDifferentiabilityWitness(DeclID DId) {
                        ArrayRef<unsigned>(parameterAndResultIndices)
                            .take_front(numParameterIndices));
   auto numResults = originalFnType->getNumResults() +
-                    originalFnType->getNumIndirectMutatingParameters();
+                    originalFnType->getNumIndirectMutatingParameters() +
+                    originalFnType->getNumYields();
   auto *resultIndices =
       IndexSubset::get(MF->getContext(), numResults,
                        ArrayRef<unsigned>(parameterAndResultIndices)
